fac58cc8a609c6f72f9147ade5fe62cf9f74c52a
sfrench <sfrench@seismo.berkeley.edu>
2013-05-26 18:05:47 -0700

    General cleanup in Optimize

--- Optimize.hs
+++ Optimize.hs
@@ -9,6 +9,8 @@ module Optimize
 import Numeric.LinearAlgebra
 import LineSearch
 
+type Vec = Vector Double
+
 -- | Learning rate for gradient descent (fixed vs. search)
 data LearnRate =
     FixedRate   Double       -- ^ Fixed learning rate (specified)
@@ -17,13 +19,13 @@ data LearnRate =
 
 -- | Gradient descent with fixed learning rate
 gradientDescent
-  :: LearnRate                         -- ^ configuration for learning rate
-  -> Double                            -- ^ residual tolerance
-  -> Int                               -- ^ maximum number of iterations
-  -> (Vector Double -> Double)         -- ^ cost function
-  -> (Vector Double -> Vector Double)  -- ^ gradient function
-  -> Vector Double                     -- ^ initial parameter estimate
-  -> (Int, Double, Vector Double)      -- ^ returns: (@niter@, @res@, @theta@)
+  :: LearnRate          -- ^ configuration for learning rate
+  -> Double             -- ^ residual tolerance
+  -> Int                -- ^ maximum number of iterations
+  -> (Vec -> Double)    -- ^ cost function
+  -> (Vec -> Vec)       -- ^ gradient function
+  -> Vec                -- ^ initial parameter estimate
+  -> (Int, Double, Vec) -- ^ returns: (@niter@, @res@, @theta@)
 gradientDescent (FixedRate alpha) tol ngditer cost grad = optimize 0
   where optimize n t = let r = cost t in
           if n == ngditer || r < tol
@@ -45,14 +47,14 @@ data CGBnType =
 
 -- | Non-linear conjugate gradient algorithm (no explicit restarts)
 conjugateGradient
-  :: CGBnType                           -- ^ configuration for beta_n term
-  -> SearchConfig                       -- ^ configuration for backtracking line search
-  -> Double                             -- ^ residual tolerance
-  -> Int                                -- ^ maximum number of iterations
-  -> (Vector Double -> Double)          -- ^ cost function
-  -> (Vector Double -> Vector Double)   -- ^ gradient function
-  -> Vector Double                      -- ^ initial parameter estimate
-  -> (Int, Double, Vector Double)       -- ^ returns: (@niter@, @res@, @theta@)
+  :: CGBnType           -- ^ configuration for beta_n term
+  -> SearchConfig       -- ^ configuration for backtracking line search
+  -> Double             -- ^ residual tolerance
+  -> Int                -- ^ maximum number of iterations
+  -> (Vec -> Double)    -- ^ cost function
+  -> (Vec -> Vec)       -- ^ gradient function
+  -> Vec                -- ^ initial parameter estimate
+  -> (Int, Double, Vec) -- ^ returns: (@niter@, @res@, @theta@)
 conjugateGradient FletcherReeves = conjugateGradientBase (\pn p -> (pn <.> pn)       / (p <.> p))
 conjugateGradient PolakRibiere   = conjugateGradientBase (\pn p -> (pn <.> (pn - p)) / (p <.> p))
 conjugateGradientBase fbn conf tol ncgiter cost grad t0 =

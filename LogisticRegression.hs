cb026bd91f9b95058eb24bd5bda26ab81d47c0fd
sfrench <sfrench@seismo.berkeley.edu>
2013-05-28 18:14:15 -0700

    Cleanup / doc updates to LogisticRegression

--- LogisticRegression.hs
+++ LogisticRegression.hs
@@ -1,7 +1,6 @@
-
--- | Logistic regression hypothesis function, as well as both normal and 
+-- | Logistic regression hypothesis function, as well as both normal and
 -- regularized variants of the cost and gradient
-module LogisticRegression 
+module LogisticRegression
 ( hypothesis
 , cost
 , gradient
@@ -12,65 +11,82 @@ module LogisticRegression
 import Numeric.LinearAlgebra
 import Foreign.Storable (Storable)
 
-ndim 
-  :: (Storable a, Num b) 
-  => Vector a 
-  -> b 
+-- Convenient type aliases
+type Vec = Vector Double
+type Mat = Matrix Double
+
+-- Tail of a Vec
+tailVector
+  :: Vec
+  -> Vec
+tailVector x = subVector 1 (dim x - 1) x
+
+-- Dimension of a vector, recast to any @Num@
+ndim
+  :: (Storable a, Num b)
+  => Vector a
+  -> b
 ndim = fromIntegral . dim
 
-sigmoid 
-  :: Vector Double 
-  -> Vector Double
+-- Logistic sigmoid
+sigmoid
+  :: Vec
+  -> Vec
 sigmoid xs = 1.0 / (1.0 + mapVector exp (-xs))
 
-hypothesis 
-  :: Matrix Double 
-  -> Vector Double 
-  -> Vector Double
+-- | Logistic regression hypothesis function
+hypothesis
+  :: Mat
+  -> Vec
+  -> Vec
 hypothesis xs theta = sigmoid $ xs <> theta
 
-cost 
-  :: Matrix Double  -- ^ examples
-  -> Vector Double  -- ^ labels
-  -> Vector Double  -- ^ parameters
-  -> Double         -- ^ returns: cost (no regularization)
-cost xs ys theta = 
+-- | Logistic regression (unregularized) cost function
+cost
+  :: Mat    -- ^ examples
+  -> Vec    -- ^ labels
+  -> Vec    -- ^ parameters
+  -> Double -- ^ returns: cost (no regularization)
+cost xs ys theta =
   let m    = ndim ys
       hs   = hypothesis xs theta
       logs = mapVector log
   in - (ys <.> logs hs + (1.0 - ys) <.> logs (1.0 - hs)) / m
-        
-gradient 
-  :: Matrix Double  -- ^ examples
-  -> Vector Double  -- ^ labels
-  -> Vector Double  -- ^ parameters
-  -> Vector Double  -- ^ returns: gradient (no regularization)
-gradient xs ys theta = 
+
+-- | Logistic regression (unregularized) cost gradient function
+gradient
+  :: Mat  -- ^ examples
+  -> Vec  -- ^ labels
+  -> Vec  -- ^ parameters
+  -> Vec  -- ^ returns: gradient (no regularization)
+gradient xs ys theta =
   let m  = ndim ys
       hs = hypothesis xs theta
       rs = hs - ys
   in trans xs <> rs / m
- 
+
+-- | Logistic regression (regularized) cost function
 costRegularized
-  :: Double 
-  -> Matrix Double 
-  -> Vector Double 
-  -> Vector Double 
-  -> Double
-costRegularized lambda xs ys theta = 
+  :: Double -- ^ regularization factor
+  -> Mat    -- ^ examples
+  -> Vec    -- ^ labels
+  -> Vec    -- ^ parameters
+  -> Double -- ^ returns: cost (with regularization)
+costRegularized lambda xs ys theta =
   let m  = ndim ys
       w  = 0.5 * lambda / m
-      ts = subVector 1 (dim theta - 1) theta
+      ts = tailVector theta
   in cost xs ys theta + w * (ts <.> ts)
- 
+
+-- | Logistic regression (regularized) cost gradient function
 gradientRegularized
-  :: Double 
-  -> Matrix Double 
-  -> Vector Double 
-  -> Vector Double 
-  -> Vector Double
-gradientRegularized lambda xs ys theta = 
+  :: Double -- ^ regularization factor
+  -> Mat    -- ^ examples
+  -> Vec    -- ^ labels
+  -> Vec    -- ^ parameters
+  -> Vec    -- ^ returns: gradient (with regularization)
+gradientRegularized lambda xs ys theta =
   let m  = ndim ys
       w  = lambda / m
-      ts = join [0.0, subVector 1 (dim theta - 1) theta]
+      ts = join [0.0, tailVector theta]
   in gradient xs ys theta + scale w ts
